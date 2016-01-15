4ca103f03c1d76bd36bc22669ee38432ddc80082
kompowiec <SstackO@o2.pl>
2016-01-15 19:52:07 +0100

    level 1

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include "ast_switch.cpp"
+using namespace std;
+
+int main()
+
+{
+int choice;
+
+	void menu();
+
+cin >> choice;
+
+
+switch( choice )
+{
+case 1:
+	void account();
+    break;
+case 2:
+	void nagroda();
+    break;
+case 3:
+	void poczta();
+    break;
+case 4:
+	void admin();
+    break;
+case 5:
+	void abonament();
+    break;
+case 6:
+    	void telefon();
+    break;
+case 7:
+    	void telefon();
+    break;
+case 8:
+	void uslugi();
+    break;
+case 9:
+    	void pogotowie();
+    break;
+default:
+    	void number();
+    break;
+}
+}
