577e995ec73e086ddbe56bb7fc8ae4315eadae04
Sorebit <cmbk90@gmail.com>
2016-09-01 15:15:21 +0200

    Added Makefile, cleaned some stuff and made it actually work

--- main.cpp
+++ main.cpp
@@ -1,50 +1,47 @@
 #include <iostream>
-#include "text.cpp"
+#include "text.h"
 using namespace std;
 
 int main()
-
 {
-int choice;
+    int choice;
 
-	void menu();
+    menu();
 
-cin >> choice;
-
-for (;;)
-{
-switch( choice )
-{
-case 1:
-	void account();
-    break;
-case 2:
-	void nagroda();
-    break;
-case 3:
-	void poczta();
-    break;
-case 4:
-	void admin();
-    break;
-case 5:
-	void abonament();
-    break;
-case 6:
-    	void telefon();
-    break;
-case 7:
-    	void telefon();
-    break;
-case 8:
-	void uslugi();
-    break;
-case 9:
-    	void pogotowie();
-    break;
-default:
-    	void number();
-    break;
-} //switch
-} //for
+    for (;;)
+    {
+        cin >> choice;
+        switch( choice )
+        {
+        case 1:
+        	account();
+            break;
+        case 2:
+        	nagroda();
+            break;
+        case 3:
+        	poczta();
+            break;
+        case 4:
+        	admin();
+            break;
+        case 5:
+        	abonament();
+            break;
+        case 6:
+            	telefon();
+            break;
+        case 7:
+            	telefon();
+            break;
+        case 8:
+        	uslugi();
+            break;
+        case 9:
+            pogotowie();
+            break;
+        default:
+            number();
+        } //switch
+    } //for
 }
