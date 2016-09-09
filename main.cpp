15e14fe4e4ac5180215debf941efa2533121a810
kompowiec <diasp@o2.pl>
2016-09-09 01:13:52 -0400

    second stage depth

--- main.cpp
+++ main.cpp
@@ -15,18 +15,29 @@ int main()
         {
         case 1:
         	account();
+1;konto();
             break;
         case 2:
         	nagroda();
+1;nagroda1();
+2;nagroda2();
+3;nagroda3();
+4;nagroda4();
+5;nagroda5();
+6;nagroda6();
             break;
         case 3:
         	poczta();
             break;
         case 4:
         	admin();
+1;admin1();
             break;
         case 5:
         	abonament();
+1;TP();
+2;train();
+3;nonfree();
             break;
         case 6:
             	telefon();
@@ -38,10 +49,11 @@ int main()
         	uslugi();
             break;
         case 9:
-            pogotowie();
+            neostrada();
+1;poczta();
             break;
         default:
-            number();
+            return 0;
         } //switch
     } //for
 }
